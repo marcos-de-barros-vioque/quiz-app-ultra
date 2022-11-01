a5e174b0bcb066d87a0df4f63c8c89fa26025db6
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-11-01 17:47:57 +0100

    added first structure

---
+++ src/components/header/Header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import "./Header.css";
+
+export default function Header() {
+    return <h1 className="Title">Quiz App Ultra</h1>;
+  }
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import "./Navigation.css";
+
+export default function Navigation() {
+    return <p className="Navi-subtitle">Quiz App Ultra</p>;
+  }
