c78cd3e93495833ccff48cf3cfe4ef324810a582
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-11-04 11:57:31 +0100

    updated card component with show answer toggle function

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import "./Card.css";
+import { useState } from "react";
 
 export default function Card() {
-    return <article className="Card-container">
-    <h2>This is the first question</h2>;
+    const [hide, setHide] = useState(false);
+    const toggleAnswer = () => {
+      setHide(!hide);
+    };
+    return (
+    <article className="Cardcontainer">
+    <h2>CSS syntax</h2>
+    <p className="Question">How does the CSS syntax look like?</p>
+    <button className="Answerbutton" onClick={toggleAnswer}>
+        {hide ? "Show Answer" : "Hide Answer"}
+      </button>
+      <p className={hide ? "hidden" : "answer"}>CSS is a rule-based language — with CSS you define the rules by specifying 
+      groups of styles that should be applied to particular elements or groups of elements on your web page.</p>
+      <ul className="Cardtags"></ul>
     </article>
+    );
   }
