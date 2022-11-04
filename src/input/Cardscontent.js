fdd5fb5c4d43d88bc0b45d3aa4f3c06bf89eced7
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-11-04 14:28:38 +0100

    added content for cards

---
+++ src/input/Cardscontent.js
@@ -0,0 +1,22 @@
+export const Cardscontent = [
+{
+question: "Is HTML an easy language?",
+answer: "HTML is the standard markup language for Web pages. With HTML you can create your own Website. HTML is easy to learn - You will enjoy it!",
+tags: ["html", "css", "js", "react"],
+},
+{
+question: "How does the CSS syntax look like?",
+answer: "CSS is a rule-based language — with CSS you define the rules by specifying groups of styles that should be applied to particular elements or groups of elements on your web page.",
+tags: ["html", "css", "js", "react"],
+},
+{
+question: "Is JavaScript a forgiving language?",
+answer: "Yes, JavaScript is a forgiving language. Some of its syntax is optional, and the language recovers from errors with more grace than many others.",
+tags: ["html", "css", "js", "react"],
+},
+{
+question: "What is React used for?",
+answer: "React is component-based javascript frontend library which is used to build the user interface especially for Single Page Application(SPA). ReactJs is not only used for web development.",
+tags: ["html", "css", "js", "react"],
+},
+];
