be8b7597390591cf64293e782229be633dadd466
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-11-07 18:53:01 +0100

    tried bookmarks feature

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,13 +2,15 @@ import React from 'react';
 import "./Card.css";
 import { useState } from "react";
 
-export default function Card({ title, question, answer }) {
+export default function Card({ title, question, answer, isBookmarked }) {
     const [hide, setHide] = useState(false);
     const toggleAnswer = () => {
       setHide(!hide);
     };
     return (
     <article className="Cardcontainer">
+      <img src={ isBookmarked === true ? "assets/bookmark-active.png" : "assets/bookmark.png" } width="40px" alt="Bookmark icon for question card">
+      </img>
        <h2>{title}</h2>
        <p>{question}</p>
        <button className="Answerbutton" onClick={toggleAnswer}>
