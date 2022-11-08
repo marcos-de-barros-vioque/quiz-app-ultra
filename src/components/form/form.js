eb339015b252f42a1b63891c3669b8985456721c
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-11-08 15:36:54 +0100

    added boomarked cards filtering feature

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./form.css";
 
-export default function Form(active) {
+export default function Form(props) {
   return (
     <section
       id="form"
-      style={{ display: active.page === "form" ? "block" : "none" }}
+      style={{ display: props.page === "form" ? "block" : "none" }}
     >
     <p>This page is under construction...</p>
     </section>
